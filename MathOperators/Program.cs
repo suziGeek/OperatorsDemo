26928e61dc7e8561b0f746f699bead6bf5c9dafb
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-06-22 15:00:22 -0400

    Add project files.

---
+++ MathOperators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            double quotient = a / b;
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder is {remainder}");
+
+            Console.WriteLine($"Enter number for radius")
+               int r = Console.ReadLine()
+
+        }
+
+        static int AreaOfCircle(r)
+        {
+           // pi* r ^2
+           Console.WriteLine($"We will give you your answer...")
+                
+
+
+       ;
+        }
+    }
+}
